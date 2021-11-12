5a099d117ea6d9cb2ee4cbbd397e1b381e431f1f
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-11-12 12:18:23 -0700

    commit tree c261dbcf3cf180093627aae98731788e14159ca4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,27 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id],
+    )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bake_good = Bakery.find(params[:id])
+    bake_good.update(
+      name: params[:name]
+    )
+    bake_good.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    bake_good = BakedGood.find(params[:id])
+    bake_good.destroy
+    bake_good.to_json
+  end
+
 end
